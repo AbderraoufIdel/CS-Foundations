d6f8e0bdc0d9051f92a4020c243f8203c4c024f1
Abderraouf_IDEL <abderraoufidel@gmail.com>
2025-06-28 15:53:30 +0100

    Chapter 02

--- python-programming-fundamentals/introduction-to-python-for-developers/Chapter-02.py
+++ python-programming-fundamentals/introduction-to-python-for-developers/Chapter-02.py
@@ -31,3 +31,51 @@ most_popular_course = most_popular_course.lower()
 print(most_popular_course)
 
 print("######END######")
+
+# Create the playlist variable
+playlist = [1, "Blinding Lights", 2, "One Dance", 3, "Uptown Funk"]
+
+# Print the list
+print(playlist)
+
+print("######END######")
+
+# Find the second song
+print(playlist[3])
+
+print(playlist[1::2])
+
+print("######END######")
+
+# Create the playlist dictionary
+playlist = {"The Weeknd": "Blinding Lights", "Drake": "One Dance"}
+
+# Print the playlist
+print(playlist)
+
+print("######END######")
+
+# Print the song by Coldplay
+print(playlist["The Weeknd"])
+
+# Add a new song
+playlist["Usher"] = "Yeah!"
+
+# Print the songs in the playlist
+print(playlist.values())
+
+print("######End######")
+
+# Create a tuple
+q3_financials = (325780, 1041, 4271599)
+
+# Print the tuple
+print(q3_financials)
+
+print("######End######")
+
+hip_hop = ["Drake", "JAY-Z", "50 Cent", "Drake"]
+hippy_set = {"dude","bruh"}
+# Create a set
+indie_set = set(hip_hop) 
+print(indie_set, hippy_set)
