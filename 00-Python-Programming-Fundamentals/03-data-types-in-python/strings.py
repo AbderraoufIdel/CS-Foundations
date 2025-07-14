a559729c74c76d97dafbf775177fef76ae06f87b
Abderraouf_IDEL <abderraoufidel@gmail.com>
2025-07-14 11:13:25 +0100

    strings

---
+++ 00-Python-Programming-Fundamentals/03-data-types-in-python/strings.py
@@ -0,0 +1,29 @@
+# Loop over top_ten_girl_names and unpack each tuple into top_ten_rank and name
+for top_ten_rank, name in top_ten_girl_names:
+  	# Print each name in the proper format
+    print(f"Rank #: {top_ten_rank} - { name }")
+
+#############################
+
+# The top ten boy names are:  as preamble
+preamble = "The top ten boy names are: "
+
+# , and as conjunction
+conjunction = ", and"
+
+# Combines the first 9 names in boy_names with a comma and space as first_nine_names
+first_nine_names = ", ".join(boy_names[0:9])
+
+# Print f-string preamble, first_nine_names, conjunction, the final item in boy_names and a period
+print(f"{preamble}{first_nine_names}{conjunction} {boy_names[-1]}.")
+
+#############################
+# Store a list of girl_names that start with s: names_with_s
+names_with_s = [name for name in girl_names if name.lower().startswith('s')]
+
+print(names_with_s)
+
+# Store a list of girl_names that contain angel: names_with_angel
+names_with_angel = [name for name in girl_names if 'angel' in name.lower()]
+
+print(names_with_angel)
